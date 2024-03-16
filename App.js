de66ddc3ec77330cb5258410cbcfa74cf1df2750
shubham-00 <srpatel980@gmail.com>
2024-03-15 22:50:16 -0400

    Image and ImageBackground components

--- App.js
+++ App.js
@@ -1,16 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	View,
+	Image,
+	ImageBackground,
+	ScrollView,
+} from 'react-native';
+
+const logoImage = require('./assets/adaptive-icon.png');
 
 export default function App() {
 	return (
-		<View style={styles.container}>
+		<ScrollView sc style={styles.container}>
 			<View style={styles.container2}></View>
 			<View style={styles.container3}></View>
 			<Text>
 				Hello <Text style={styles.text2}>World</Text>
 			</Text>
-			<StatusBar style='auto' />
-		</View>
+			{/* image from assets directory */}
+			<Image source={logoImage} style={styles.image} />
+			{/* image coming through network (height and width are required) */}
+			<Image
+				source={{ uri: 'https://picsum.photos/300' }}
+				style={styles.image}
+			/>
+			{/* image as a background */}
+			<ImageBackground
+				source={logoImage}
+				style={{ flex: 1, width: '100%' }}
+				resizeMode={'cover'}>
+				<Text styles={styles.text2}>This is some text</Text>
+				<Text styles={styles.text2}>This is some text</Text>
+				<Text styles={styles.text2}>This is some text</Text>
+			</ImageBackground>
+		</ScrollView>
 	);
 }
 
@@ -18,7 +41,8 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
-		paddingTop: 35,
+		// paddingTop: 35,
+		// paddingBottom: 350,
 		// alignItems: 'center',
 		// justifyContent: 'center',
 	},
@@ -34,5 +58,10 @@ const styles = StyleSheet.create({
 	},
 	text2: {
 		color: 'pink',
+		height: 20,
+	},
+	image: {
+		height: 300,
+		width: 300,
 	},
 });

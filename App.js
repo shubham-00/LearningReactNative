1f038ee7075b865168be7c5573d2c4ac8c687417
shubham-00 <srpatel980@gmail.com>
2024-03-15 22:55:18 -0400

    ScrollView component

--- App.js
+++ App.js
@@ -11,29 +11,31 @@ const logoImage = require('./assets/adaptive-icon.png');
 
 export default function App() {
 	return (
-		<ScrollView sc style={styles.container}>
-			<View style={styles.container2}></View>
-			<View style={styles.container3}></View>
-			<Text>
-				Hello <Text style={styles.text2}>World</Text>
-			</Text>
-			{/* image from assets directory */}
-			<Image source={logoImage} style={styles.image} />
-			{/* image coming through network (height and width are required) */}
-			<Image
-				source={{ uri: 'https://picsum.photos/300' }}
-				style={styles.image}
-			/>
-			{/* image as a background */}
-			<ImageBackground
-				source={logoImage}
-				style={{ flex: 1, width: '100%' }}
-				resizeMode={'cover'}>
-				<Text styles={styles.text2}>This is some text</Text>
-				<Text styles={styles.text2}>This is some text</Text>
-				<Text styles={styles.text2}>This is some text</Text>
-			</ImageBackground>
-		</ScrollView>
+		<View style={{ ...styles.container, padding: 50 }}>
+			<ScrollView>
+				<View style={styles.container2}></View>
+				<View style={styles.container3}></View>
+				<Text>
+					Hello <Text style={styles.text2}>World</Text>
+				</Text>
+				{/* image from assets directory */}
+				<Image source={logoImage} style={styles.image} />
+				{/* image coming through network (height and width are required) */}
+				<Image
+					source={{ uri: 'https://picsum.photos/300' }}
+					style={styles.image}
+				/>
+				{/* image as a background */}
+				<ImageBackground
+					source={logoImage}
+					style={{ flex: 1, width: '100%' }}
+					resizeMode={'cover'}>
+					<Text styles={styles.text2}>This is some text</Text>
+					<Text styles={styles.text2}>This is some text</Text>
+					<Text styles={styles.text2}>This is some text</Text>
+				</ImageBackground>
+			</ScrollView>
+		</View>
 	);
 }
 
